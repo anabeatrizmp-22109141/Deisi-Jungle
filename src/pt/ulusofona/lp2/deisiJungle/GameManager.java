9c0ba3df8f4bfc6dfff48b6bc78fb103ddf5a06e
Ana Beatriz Pires <abmpires1999@gmail.com>
2022-10-28 16:57:57 +0100

    Modificação do getSpecies()

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -1,5 +1,4 @@
 package pt.ulusofona.lp2.deisiJungle;
-
 import javax.swing.*;
 import java.util.ArrayList;
 
@@ -8,7 +7,29 @@ public class GameManager {
     }
 
     public String[][] getSpecies() {
-        return new String[1][1];
+        String[][] especies = new String[5][3];
+
+        especies[0][0] = "E";
+        especies[0][1] = "Elefante";
+        especies[0][2] = "elephant.png";
+
+        especies[1][0] = "L";
+        especies[1][1] = "Leão";
+        especies[1][2] = "lion.png";
+
+        especies[2][0] = "T";
+        especies[2][1] = "Tartaruga";
+        especies[2][2] = "turtle.png";
+
+        especies[3][0] = "P";
+        especies[3][1] = "Pássaro";
+        especies[3][2] = "bird.png";
+
+        especies[4][0] = "Z";
+        especies[4][1] = "Tarzan";
+        especies[4][2] = "tarzan.png";
+
+        return especies;
     }
 
     public boolean createInitialJungle(int jungleSize, int initialEnergy, String[][] playersInfo) {
